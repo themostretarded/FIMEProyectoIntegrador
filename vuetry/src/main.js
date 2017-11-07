9eb62109438b1a631da01de154bc9840be16c14e
themostretarded <marcosresidentevil@hotmail.com>
2017-11-06 19:38:40 -0600

    son mamadas

--- vuetry/src/main.js
+++ vuetry/src/main.js
@@ -1,12 +1,13 @@
 import Vue from 'vue'
 import App from './App'
+import pinchecomponenteculero from './pinchecomponenteculero'
 import router from './router'
 
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
 new Vue({
-  el: '#app',
+  el: '#pinchecomponenteculero',
   router,
-  render: h => h(App)
+  render: h => h(pinchecomponenteculero)
 })
---
+++ vuetry/src/pinchecomponenteculero.vue
@@ -0,0 +1,22 @@
+<template>
+  <div id="pinchecomponenteculero">
+    <router-view/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'pinchecomponenteculero'
+}
+</script>
+
+<style>
+#pinchecomponenteculero {
+  font-family: 'Verdana', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3ef0;
+  margin-top: 60px;
+}
+</style>
