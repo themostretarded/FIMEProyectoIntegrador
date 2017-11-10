cb25c0a9c0eb93bf6f93024ec4df02aeaf01707a
themostretarded <marcosresidentevil@hotmail.com>
2017-11-09 18:36:48 -0600

    Script generador ya genera cards con id
    faltan las cards

--- Normal/js/generador.js
+++ Normal/js/generador.js
@@ -1,7 +1,25 @@
-$(()=>{
+var conteo = 0;
+var veces = 0;
+var filas = 0;
+var r = "0";
+$(() => {
     $("#agregar").click(() => {
-        $("#contenido").append("<div class=\"w3-card\"><p>w3-card</p></div>")
-      });
+
+        if (conteo == 0) {
+            $("#contenedor").append("<div class=\"w3-row\" id=\"contenido" + filas + "\"></div>");
+            filas = filas + 1;
+        }
+        
+        $("#"+"contenido" + r).append("<div class=\"w3-col m3 l2\"><div class=\"w3-card\"><p>w3-card</p></div></div>");
+        conteo = conteo + 1;
+        if (conteo == 6) {
+            conteo = 0;
+            r = filas.toString()
+        }
+        veces = veces + 1;
+        console.log("conteo:" + conteo + "\n veces:" + veces);
+        console.log("#contenido" + r);
+    });
 })
 /*Funciones para la barra normal*/
 function w3_open() {
