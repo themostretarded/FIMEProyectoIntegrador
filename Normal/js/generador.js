6b1cbd7e318dfebefb493a834c83177d844f5c20
themostretarded <marcosresidentevil@hotmail.com>
2017-11-21 21:25:10 -0600

    Ya jala el cambio
    ¬¬ a medias solo los pares los cambia
    falta el guardado de las mesas en un objeto y quedaria lista la pagina web para su fase 1

--- Normal/js/generador.js
+++ Normal/js/generador.js
@@ -5,14 +5,15 @@ var arr_mesas = [];
 var r = "0";
 
 $(() => {
+    //agrega las mesas y las separa por filas
     $("#agregar").click(() => {
 
         if (conteo == 0) {
             $("#contenedor").append("<div class=\"w3-row\" id=\"contenido" + filas + "\"></div>");
             filas = filas + 1;
         }
-        if(veces == 1){
-            $("#guardando").append("<button class=\"w3-btn w3-block w3-teal w3-animate-opacity\">Guardar</button>");
+        if (veces == 1) {
+            $("#guardando").append("<button id=\"btnguardar\" class=\"w3-btn w3-block w3-teal w3-animate-opacity\">Guardar</button>");
         }
 
         $("#" + "contenido" + r).append("<div class=\"w3-col m3 l2\"><div class=\"w3-card w3-center w3-pale-green w3-animate-zoom w3-tooltip\" id=" + veces + "><p><img src=\"./media/icono/table.png\" alt=\"MesaQueMasApluada\" class=\"mesas\"><div><span>Estado</span><span class=\"w3-text w3-tag w3-animate-opacity\">De click para cambiar el estado<span></div></p><p>Numero:" + veces + "</p></div></div>");
@@ -22,12 +23,18 @@ $(() => {
             r = filas.toString()
         }
         veces = veces + 1;
+        
         var obj_mesa = { id: veces, isOcupado: false };
         arr_mesas.push(obj_mesa);
         console.log('json_mesas:');
         console.log(arr_mesas);
+        
+        $( "div.w3-card" ).on( "click", function() {
+            $(this).toggleClass("w3-pale-green w3-pale-red");
+          });
     });
 
+    //quita mesas por medio de su id
     $(".enviarid").click(() => {
         var elimar = $("#numero").val();
         console.log(elimar);
@@ -44,12 +51,23 @@ $(() => {
         selimar = elimar.toString();
         $("#" + selimar).parent().remove();
     });
-    
-    $("div.w3-card").click(() => {
-        $("div").removeClass( "w3-pale-green" );
-    });
 
-})
+    //guardar en json al dar click en boton
+    $("#btnguardar").click(()=>{
+        var json = JSON.stringify( arr_mesas );
+        console.log(json);
+    })
+
+    /*cambia el estado de las mesas
+    $("div.w3-card").click(() => {
+        $(this).removeClass("w3-pale-green").addClass("w3-pale-red");
+    });
+    */
+    
+      
+
+});
+
 /*Funciones para la barra normal*/
 function w3_open() {
     document.getElementById("mySidebar").style.display = "block";
