20a240ebf7336c0d766bb18f80bdaa447bc604f2
themostretarded <marcosresidentevil@hotmail.com>
2017-11-27 16:16:24 -0600

    tratando de usar una bd

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var app = express();
 app.get('/',  (req, res) => {
-    res.sendFile(__dirname + '/index.html')
+    res.sendFile(__dirname + '/index.html');
 });
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!');
